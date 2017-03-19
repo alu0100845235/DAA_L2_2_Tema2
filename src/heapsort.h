abcb100cf4f71a6fd8c974b074475623bee89da3
Airam Navas <alu0100618426@ull.edu.es>
2017-03-19 23:16:30 +0000

    Comentando y modificando heapSort

--- src/heapsort.h
+++ src/heapsort.h
@@ -6,23 +6,37 @@
 
 using namespace std;
 
+/**
+* Declaración de la clase heapSort
+*/
 template <class T>
 class heapSort {
 	public:
 		heapSort();
 		virtual ~heapSort();
 		int sort(vector<T>& values, bool debug = false);
-		int baja(int i, vector<T>& values, int size, bool debug);
-		ostream& debugValues(ostream& os, vector<T>& values, int size, int j, int k);
-		ostream& debugValues2(ostream& os, vector<T>& values, int ini, int fin, int ins);
+		int sink(int i, vector<T>& values, int size, bool debug);
+		ostream& debugSwap(ostream& os, vector<T>& values, int size, int j, int k);
+		ostream& debugToStudy(ostream& os, vector<T>& values, int ini, int fin, int ins);
 };
 
+/**
+* Constructor vacío de la clase heapSort
+*/
 template <class T>
 heapSort<T>::heapSort() {}
 
+/**
+* Destructor de la clase heapSort
+*/
 template <class T>
 heapSort<T>::~heapSort() {}
 
+/**
+* Método sort que ejecuta el algoritmo heapSort
+* principal. Creando el árbol, y modificandolo
+* según los pasos del algoritmo
+*/
 template <class T>
 int heapSort<T>::sort(vector<T>& values, bool debug) {
 	int size = values.size();
@@ -30,24 +44,29 @@ int heapSort<T>::sort(vector<T>& values, bool debug) {
 
 	for (int i = (size / 2); i > 0; --i) {
 		if (debug)
-			debugValues2(cout, values, i, size - 1, 1);
-		steps += baja(i, values, size, debug);
+			debugToStudy(cout, values, i, size - 1, 1);
+		steps += sink(i, values, size, debug);
 	}
 	for (int i = size; i > 1; --i){
 		T x = values[0];
 		values[0] = values[i-1];
 		values[i-1] = x;
 		if (debug)
-			debugValues2(cout, values, 0, i-2, 0);
-		steps += baja(1, values, i-1, debug);
+			debugToStudy(cout, values, 0, i-2, 0);
+		steps += sink(1, values, i-1, debug);
 	}
 	if (debug)
-		debugValues(cout, values, size, -1, -1);
+		debugSwap(cout, values, size, -1, -1);
 	return steps;
 }
 
+/**
+* Método sink que se encarga de intercambiar
+* los elementos necesarios a la hora de
+* reorganizar el árbol
+*/
 template <class T>
-int heapSort<T>::baja(int i, vector<T>& values, int size, bool debug) {
+int heapSort<T>::sink(int i, vector<T>& values, int size, bool debug) {
 	int steps = 0;
 	T x = values[i-1];
 	int h, h1, h2;
@@ -57,7 +76,7 @@ int heapSort<T>::baja(int i, vector<T>& values, int size, bool debug) {
 		h2 = h1 + 1;
 
 		if (debug && h1 != size) {
-			debugValues(cout, values, size, h1-1, h2-1);
+			debugSwap(cout, values, size, h1-1, h2-1);
 		}
 		if (h1 == size)
 			steps++;
@@ -68,7 +87,7 @@ int heapSort<T>::baja(int i, vector<T>& values, int size, bool debug) {
 			h = h2;
 
 		if (debug)
-			debugValues(cout, values, size, h-1, i-1);
+			debugSwap(cout, values, size, h-1, i-1);
 		steps++;
 
 		if (values[h-1] <= x)
@@ -82,8 +101,13 @@ int heapSort<T>::baja(int i, vector<T>& values, int size, bool debug) {
 	return steps;
 }
 
+/**
+* Método que imprime los elementod del
+* vector y muestra los elementos sobre
+* los que se realizará el swap
+*/
 template <class T>
-ostream& heapSort<T>::debugValues(ostream& os, vector<T>& values, int size, int j, int k) {
+ostream& heapSort<T>::debugSwap(ostream& os, vector<T>& values, int size, int j, int k) {
 	for (int i = 0; i < size; i++) {
 		if ((i != j) && (i != k))
 			os << values[i] << " ";
@@ -95,8 +119,13 @@ ostream& heapSort<T>::debugValues(ostream& os, vector<T>& values, int size, int 
 	return os;
 }
 
+/**
+* Método que imprime los elementos del
+* vector subrayando los elementos a
+* "estudiar"
+*/
 template <class T>
-ostream& heapSort<T>::debugValues2(ostream& os, vector<T>& values, int ini, int fin, int ins) {
+ostream& heapSort<T>::debugToStudy(ostream& os, vector<T>& values, int ini, int fin, int ins) {
 	int size = values.size();
 
 	if (ins == 1) {
